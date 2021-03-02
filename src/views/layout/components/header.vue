8082f6ed5dc2a171252c714b5806a81cc145c8fd
alex <1173683665@qq.com>
2021-03-02 23:15:32 +0800

    代代码优化

---
+++ src/views/layout/components/header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main-header">
+    <a-button
+      type="primary"
+      style="margin-bottom: 16px"
+      @click="toggleCollapsed"
+    >
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadCrumb">
+      <a-breadcrumb>
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <ul class="user-info">
+      <li>
+        Welcome,Alex
+        <a-icon type="down" />
+      </li>
+      <li>Log Out</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
