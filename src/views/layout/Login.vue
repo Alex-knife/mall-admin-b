d63c36e48db7ed2c3cc3e4173216ae0ffe236b5c
alex <1173683665@qq.com>
2021-03-03 12:40:23 +0800

    login-1

---
+++ src/views/layout/Login.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="login">
+    <a-form-model
+      class="login-form"
+      ref="loginForm"
+      :model="loginForm"
+      :rules="rules"
+      v-bind="layout"
+    >
+      <a-form-model-item has-feedback label="Email" prop="email">
+        <a-input v-model="loginForm.email" />
+      </a-form-model-item>
+      <a-form-model-item has-feedback label="Password" prop="password">
+        <a-input
+          v-model="loginForm.password"
+          type="password"
+          autocomplete="off"
+        />
+      </a-form-model-item>
+      <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button type="primary" @click="submitForm('loginForm')">
+          提交
+        </a-button>
+        <a-button style="margin-left: 10px" @click="resetForm('loginForm')">
+          重置
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+<script>
+import api from '@/api/user';
+
+export default {
+  data() {
+    const emailReg = /^[\w-]+@[\w.-]+.com$/;
+    const checkEmail = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('Please input your email'));
+      }
+      if (emailReg.test(value)) {
+        return callback();
+      }
+      return callback(new Error('Wrong Email Format'));
+    };
+    const validatePassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input the password'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      loginForm: {
+        email: '',
+        password: '',
+      },
+      rules: {
+        email: [{ validator: checkEmail, trigger: 'change' }],
+        password: [{ validator: validatePassword, trigger: 'change' }],
+      },
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 },
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // 传入数据
+          api.login(this.loginForm).then((res) => {
+            console.log(res);
+            // 页面跳转
+            this.$router.push({
+              name: 'Home',
+            });
+          }).catch((error) => {
+            this.$message.error(error);
+          });
+          return true;
+        }
+        console.log('error submit!!');
+        return false;
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
+
+<style lang="less">
+@import url("~@/assets/css/login.less");
+</style>
