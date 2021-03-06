1f4588066bceba7a282e2e207f2fee8c5e51e4a9
alex <1173683665@qq.com>
2021-03-06 19:16:20 +0800

    商品列表1

---
+++ src/components/search.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="search">
+    <a-form-model
+      layout="inline"
+      :model="searchForm"
+      @submit="handleSubmit"
+      @submit.native.prevent
+    >
+      <a-form-model-item label="搜索关键字：">
+        <a-input v-model="searchForm.searchWord" placeholder="请输入关键字">
+        </a-input>
+      </a-form-model-item>
+      <a-form-model-item label="商品类目：">
+        <a-select
+          show-search
+          placeholder="请选择商品类目"
+          style="width: 200px"
+          @change="handleChange"
+          allowClear
+        >
+          <a-select-option
+            v-for="c in data"
+            :key="c._id"
+            :value="c.id"
+            >{{ c.name }}</a-select-option
+          >
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-button type="primary" html-type="submit"> Search </a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      searchForm: {
+        searchWord: '',
+        category: '',
+      },
+      categoryList: [],
+    };
+  },
+  props: ['data'],
+  methods: {
+    handleSubmit() {
+      this.$emit('submit', this.sea);
+    },
+    handleChange(val) {
+      this.searchForm.category = val;
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+    .search{
+        padding: 10px 40px;
+    }
+</style>
--- src/views/pages/productList.vue
+++ src/views/pages/productList.vue
@@ -1,3 +1,81 @@
 <template>
-    <div>商品列表</div>
+  <div class="product-list">
+    <!-- 搜索部分 -->
+    <search @submit="searchSubmit" :data="categoryList" />
+    <!-- 表格显示部分 -->
+    <!-- 绑定data对应:data-source="tableData" -->
+    <!-- categoryList映射到productTable -->
+    <product-list
+      :data="tableData"
+      :page="page"
+      @change="changePage"
+      :categoryList="categoryList"
+    >
+    </product-list>
+  </div>
 </template>
+
+<script>
+import Search from '@/components/search.vue';
+import ProductList from '@/components/productTable.vue';
+import api from '@/api/product';
+import categoryApi from '@/api/category';
+
+export default {
+  data() {
+    return {
+      categoryList: [],
+      tableData: [],
+      // 搜索值
+      searchForm: {},
+      page: {
+        current: 1,
+        pageSize: 10,
+        showSizeChanger: true,
+        total: 1,
+      },
+      categoryObj: {},
+    };
+  },
+  components: {
+    Search,
+    ProductList,
+  },
+  // 获取数据的先后 【先获取类目 在获取表格数据】
+  async created() {
+    await categoryApi.list().then((res) => {
+      this.categoryList = res.data;
+      res.data.forEach((item) => {
+        this.categoryObj[item.id] = item;
+      });
+    });
+    this.getTableData();
+  },
+  methods: {
+    searchSubmit(form) {
+      this.searchForm = form;
+    },
+    getTableData() {
+      api
+        .list({
+          page: this.page.current,
+          size: this.page.pageSize,
+          ...this.searchForm,
+        })
+        .then((res) => {
+          console.log(res);
+          this.page.total = res.total;
+          // 从api成功获得 数据传给表格 数组形式
+          this.tableData = res.data.map((item) => ({
+            ...item,
+            categoryName: this.categoryObj[item.category].name,
+          }));
+        });
+    },
+    changePage(page) {
+      this.page = page;
+      this.getTableData();
+    },
+  },
+};
+</script>
